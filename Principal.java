e472f0be1ccfbd9734f5386a4fddaf2efe9d05ad
Lívia Pereira Noronha <101676830+Livianoronhaa@users.noreply.github.com>
2024-04-05 14:29:22 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,17 +1,24 @@
 public class Principal {
 
     public static void main(String[] args) {
-        Carro carros; 
-        Moto motos;
-
-        carros = new Carro("Toyota", "Corolla", 2020, "preto", 4);
-        motos = new Moto("Honda", "CBR500R", 2021, "Vermelho", 500);
-		
-        System.out.println("Informações do Carro:");
-        System.out.println(carros.descricao());
+    
+        Carro carros = new Carro("Toyota", "Corolla", 2020, "preto", 4);
+        Carro carro2 = new Carro("Fiat", "Palio", 2015, "Branco", 4);
+        Carro carro3 = new Carro("Fiat", "Uno", 2009, "Vermelho", 2);
         
-        System.out.println("\nInformações da Moto:");
+        Moto motos = new Moto("Honda", "CBR500R", 2021, "Vermelho", 500);
+        Moto moto2 = new Moto("Yamarra", "LALALA", 2017, "Preto", 500);
+        Moto moto3 = new Moto("Honda", "Fan", 2023, "Branco", 500);
+		
+        System.out.println("Informações dos carros:");
+        System.out.println(carros.descricao());
+        System.out.println(carro2.descricao());
+        System.out.println(carro3.descricao());
+        
+        System.out.println("\nInformações das motos:");
         System.out.println(motos.descricao());
+        System.out.println(moto2.descricao());
+        System.out.println(moto3.descricao());
     }
 
 }
