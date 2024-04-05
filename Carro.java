f7246d53da1080f0922a8fef4ddba4b71fb7a1f8
Lívia Pereira Noronha <101676830+Livianoronhaa@users.noreply.github.com>
2024-04-05 14:29:46 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,12 +1,13 @@
 class Carro {
-    String marca;
-    String modelo;
-    int ano;
-    String cor;
-    int numero_portas;
+    public String marca;
+    public String modelo;
+    public int ano;
+    public String cor;
+    public int numero_portas;
 
     public Carro() {}
 
+    //Construtor 
     public Carro(String marca, String modelo, int ano, String cor, int numero_portas) {
         this.marca = marca;
         this.modelo = modelo;
